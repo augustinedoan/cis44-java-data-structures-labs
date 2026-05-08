2842e77a8af636595d91dacfecc6e392a23103c2
augustinedoan <augustine.doan@gmail.com>
2026-05-08 13:21:20 -0700

    Add files via upload

---
+++ lab14-maps-hash-tables/lab14-unsorted-list-map/Entry.java
@@ -0,0 +1,20 @@
+package com.mycompany.unsortedlistmap;
+
+public class Entry<K, V> {
+    private final K key;
+    private V value;
+
+    public Entry(K key, V value) {
+        this.key = key;
+        this.value = value;
+    }
+
+    public K getKey() { return key; }
+    public V getValue() { return value; }
+
+    public V setValue(V value) {
+        V old = this.value;
+        this.value = value;
+        return old;
+    }
+}
---
+++ lab14-maps-hash-tables/lab14-unsorted-list-map/UnsortedListMap.java
@@ -0,0 +1,45 @@
+package com.mycompany.unsortedlistmap;
+
+import java.util.ArrayList;
+
+public class UnsortedListMap<K, V> implements MapADT<K, V> {
+    private ArrayList<Entry<K, V>> list = new ArrayList<>();
+
+    private int findEntryIndex(K key) {
+        for (int i = 0; i < list.size(); i++) {
+            if (list.get(i).getKey().equals(key)) {
+                return i;
+            }
+        }
+        return -1;
+    }
+
+    public int size() { return list.size(); }
+    public boolean isEmpty() { return list.isEmpty(); }
+
+    public V get(K key) {
+        int i = findEntryIndex(key);
+        return (i != -1) ? list.get(i).getValue() : null;
+    }
+
+    public V remove(K key) {
+        int i = findEntryIndex(key);
+        if (i != -1) {
+            V oldValue = list.get(i).getValue();
+            list.remove(i);
+            return oldValue;
+        }
+        return null;
+    }
+
+    public V put(K key, V value) {
+        int i = findEntryIndex(key);
+
+        if (i != -1) {
+            return list.get(i).setValue(value);
+        }
+
+        list.add(new Entry<>(key, value));
+        return null;
+    }
+}
